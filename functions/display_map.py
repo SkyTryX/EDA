3db940b7860bd87c9564e308b2f398ee51968f84
Flowzyzz <147387521+Flowzyzz@users.noreply.github.com>
2024-03-05 15:11:05 +0100

    def de deplacement pour controler si un bot peut bouger ou non

--- functions/display_map.py
+++ functions/display_map.py
@@ -25,3 +25,17 @@ def load_map(map_csv):
     return model
 
 
+def deplacemennt(dico, position_bot : list[tuple], namebot):
+    """
+    Renvoi le dico modifié si le bot peut se déplacer au bon endroit, sinon renvoi le même dico
+    exemple de fonction ecrite : deplacement( load_map(map1.csv), (2,6), '1' )
+    """
+    for x in dico['wall'][0]:
+        # manque de définir pour que le bot sorte pas de la map
+        if position_bot == x:
+            return dico, False
+        else:
+            dico[namebot] = [position_bot]
+            return dico, True
+
+
