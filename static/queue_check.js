c7d39c7d6ed4ec688470aa7535d8f9e460703b44
SkyTryX <dorian.gil34@gmail.com>
2024-03-28 09:15:37 +0100

    edits fix interval

--- static/queue_check.js
+++ static/queue_check.js
@@ -2,21 +2,22 @@ async function getJSON() {
     return await (await fetch("api/queue")).json();
 }
 
-setInterval(function () {
+const interval_chrono = setInterval(chrono, 1000);
+
+function chrono() {
     data = getJSON();
     data.then(function (result) {
         site = document.getElementsByTagName('html')[0].innerHTML;
         if (site.includes("course")) {
             if (result["course"] === "None") {
                 location.href = '/course';
-                clearInterval(this);
+                clearInterval(interval_chrono);
             }
         } else if (site.includes("combat")) {
             if (result["combat"] === "None") {
                 location.href = '/combat';
-                clearInterval(this);
+                clearInterval(interval_chrono);
             }
         }
     });
-}, 1000);
-
+}
